68a9d81fd7946891fdddbb1fe9ba7c65645a1fc9
Foch01 <tsanceo@yahoo.com>
2019-11-26 15:00:46 +0100

    feat(SEL-9): delete email

--- src/app/main/products/product/product.component.ts
+++ src/app/main/products/product/product.component.ts
@@ -7,33 +7,6 @@ import { locale as french } from '../i18n/fr';
 import { ProductService } from './product.service';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
-const email: any[] = [
-    {
-        'name': 'Email n°1',
-        'numberProduct': 3,
-        'declenchement': 'Envoyer l’email pour les commandes réalisées 2 Jours après Livraison',
-        'active': 'Actif',
-    },
-    {
-        'name': 'Email n°2',
-        'numberProduct': 2,
-        'declenchement': 'Envoyer l’email pour les commandes réalisées 2 Jours après Livraison',
-        'active': 'Actif',
-    },
-    {
-        'name': 'Email n°3',
-        'numberProduct': 1,
-        'declenchement': 'Envoyer l’email pour les commandes réalisées 2 Jours après Livraison',
-        'active': 'Actif',
-    },
-    {
-        'name': 'Email n°4',
-        'numberProduct': 6,
-        'declenchement': 'Envoyer l’email pour les commandes réalisées 2 Jours après Livraison',
-        'active': 'Actif',
-    }
-];
-
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html',
