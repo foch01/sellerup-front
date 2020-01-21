5c34a199d2ca7adce479cedde31998b43cdc7294
Sagory <quentin0853@msn.com>
2020-01-21 23:06:53 +0100

    feat(SEL-15):get_data_formgroup

--- src/app/main/create-email/create-email.component.ts
+++ src/app/main/create-email/create-email.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-email',
@@ -13,22 +14,94 @@ export class CreateEmailComponent implements OnInit {
   public Editor = ClassicEditor;
   public dataCkeEditor: any;
 
-  // Ecoute si un ajout ou une modification de données e eu lieu
+  // Ecoute si un ajout ou une modification de données a eu lieu
   public onChange( { editor }: ChangeEvent ) {
     this.dataCkeEditor = editor.getData();
     console.log(this.dataCkeEditor);
-}
+  }
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
   }
+  
+  public formGroup: FormGroup = this._formBuilder.group({
 
-  public handleSaveForm() {
-    console.log(this.dataCkeEditor)
+    title: new FormControl(null, {
+        validators: [
+            Validators.required,
+        ],
+        updateOn: 'change',
+    }),
 
+    subject: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul1: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul2: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+    deroul3: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul4: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul5: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+    deroul6: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+  });
+
+  public async handleOnSubmit() {
+    const payload = {
+              title: this.formGroup.value.title,
+              subject: this.formGroup.value.subject,
+              content: this.dataCkeEditor,
+              deroul1: this.formGroup.value.deroul1,
+              deroul2: this.formGroup.value.deroul2,
+              deroul3: this.formGroup.value.deroul3,
+              deroul4: this.formGroup.value.deroul4,
+              deroul5: this.formGroup.value.deroul5,
+              deroul6: this.formGroup.value.deroul6,
+          };
+    console.log(payload);
   }
-
 }
 
+​
 
