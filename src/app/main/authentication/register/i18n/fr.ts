faf209cbc622d72dec1f55b93f30ad89f7522ae7
leneveuT <leneveu.thibaut@gmail.com>
2020-05-07 14:25:56 +0200

    Add the translations for the intros

--- src/app/main/authentication/register/i18n/fr.ts
+++ src/app/main/authentication/register/i18n/fr.ts
@@ -1,6 +1,10 @@
 export const locale = {
     lang: 'fr',
     data: {
+        INTRO: {
+            TITLE: 'Bienvenue sur SellerUp !',
+            DESCRIPTION: 'C\'est l\'outil qui vous accompagne au quotidien pour la gestion Marketing et Logistique de votre boutique Amazon FBA.'
+        },
         REGISTER: {
             TITLE: 'CRÉATION DE VOTRE COMPTE',
         },
