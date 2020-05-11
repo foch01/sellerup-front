fe5b471cdb672e630ed65cea903694be4aa84277
leneveuT <leneveu.thibaut@gmail.com>
2020-05-11 11:37:32 +0200

    Add the email component

---
+++ src/app/main/create-email/create-email.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-email',
+  templateUrl: './create-email.component.html',
+  styleUrls: ['./create-email.component.scss']
+})
+
+export class CreateEmailComponent implements OnInit {
+  demo = true;
+  key = 1;
+  workflows = [];
+
+  public Editor = ClassicEditor;
+  public dataCkeEditor: any;
+
+  // Ecoute si un ajout ou une modification de données a eu lieu
+  public onChange( { editor }: ChangeEvent ) {
+    this.dataCkeEditor = editor.getData();
+    console.log(this.dataCkeEditor);
+  }
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.demo = false; 
+    this.key = 1;
+    this.workflows = [
+      {
+        key: 1,
+        value: "Gestion des commandes"
+      },
+      { 
+        key: 2,
+        value: "Gestion du marketing"
+      }
+    ]
+  }
+
+  updateSelect(event) {
+    this.key = event.target.value;
+    console.log(this.key == 1);
+    console.log(typeof(this.key));
+  }
+
+  ngOnInit() {
+  }
+  
+  public formGroup: FormGroup = this._formBuilder.group({
+
+    title: new FormControl(null, {
+        validators: [
+            Validators.required,
+        ],
+        updateOn: 'change',
+    }),
+
+    subject: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul1: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul2: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+    deroul3: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul4: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+
+    deroul5: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+    deroul6: new FormControl(null, {
+      validators: [
+          Validators.required,
+      ],
+      updateOn: 'change',
+    }),
+    
+  });
+
+  public async handleOnSubmit() {
+    const payload = {
+              title: this.formGroup.value.title,
+              subject: this.formGroup.value.subject,
+              content: this.dataCkeEditor,
+              deroul1: this.formGroup.value.deroul1,
+              deroul2: this.formGroup.value.deroul2,
+              deroul3: this.formGroup.value.deroul3,
+              deroul4: this.formGroup.value.deroul4,
+              deroul5: this.formGroup.value.deroul5,
+              deroul6: this.formGroup.value.deroul6,
+          };
+    console.log(payload);
+  }
+}
+
+​
+
---
+++ src/app/main/create-email/create-email.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import { CreateEmailComponent } from './create-email.component';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatStepperModule, MatCheckboxModule } from '@angular/material';
+
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+const routes = [
+  {
+      path     : 'create-email',
+      component: CreateEmailComponent
+  }
+];
+
+@NgModule({
+  declarations: [CreateEmailComponent],
+  imports: [
+    RouterModule.forChild(routes),
+
+    TranslateModule,
+
+    FuseSharedModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatCheckboxModule,
+
+    CKEditorModule,
+
+  ],
+  exports : [
+    CreateEmailComponent,
+]
+  
+})
+
+export class CreateEmailModule { }
+
